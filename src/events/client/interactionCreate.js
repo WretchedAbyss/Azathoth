4dece4cce983310ac0a6082cb1777d8067229a34
WretchedAbyss <AlecJamesFowler@protonmail.com>
2023-01-13 19:42:07 -0500

    First Commit

---
+++ src/events/client/interactionCreate.js
@@ -0,0 +1,24 @@
+const { once } = require("events");
+
+module.exports = {
+    name : 'interactionCreate',
+    async execute(interaction, client) {
+        if (interaction.isChatInputCommand()) {
+            const { commands } = client;
+            const { commandName } = interaction;
+            const command = commands.get(commandName);
+            if (!command) return;
+
+            try {
+                await command.execute(interaction, client);
+
+            } catch (error) {
+                console.error(error);
+                await interaction.reply({
+                    content :`Something Went Wrong while executing this command`,
+                    ephemeral: true
+                });
+            }
+        }
+    }
+}
---
+++ src/events/client/ready.js
@@ -0,0 +1,9 @@
+const { once } = require("events");
+
+module.exports = {
+    name : 'ready',
+    once: true,
+    async execute(client) {
+        console.log(`Ready to go ${client.user.tag} is logged in`)
+    }
+}
